0c4cb03530d0ab4dccf972495b00ade5d1bbda81
Rei Lin <temisrei@gmail.com>
2020-09-11 13:57:30 +0800

    加入 jest，將手動測試改為 jest

--- src/index.test.js
+++ src/index.test.js
@@ -1,10 +1,25 @@
 const benchmarker = require('./index');
 
-function jsonStringify() {
-  JSON.stringify({
-    foo: 'bar',
-  });
-}
+describe('test function-benchmarker', () => {
+  it('run default times (1000000)', () => {
+    const mockFn = jest.fn();
+    const costTime = benchmarker(mockFn);
 
-const costTime = benchmarker(jsonStringify);
-console.log('costTime', costTime);
+    expect(mockFn).toHaveBeenCalledTimes(1000000);
+    expect(costTime).toEqual(expect.any(Number));
+  });
+
+  it('run with 1000 times', () => {
+    const mockFn = jest.fn();
+    const costTime = benchmarker(mockFn, 1000);
+
+    expect(mockFn).toHaveBeenCalledTimes(1000);
+    expect(costTime).toEqual(expect.any(Number));
+  });
+
+  it('throw error when not provide a function', () => {
+    const mockFn = 'THIS_IS_NOT_A_FUNCTION';
+    expect(() => benchmarker(mockFn)).toThrow();
+  })
+
+});
