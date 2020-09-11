892b5a2d28ff3254137ca70ddebdb6c67146af29
Rei Lin <temisrei@gmail.com>
2020-09-11 10:04:10 +0800

    加入主程式

---
+++ src/index.js
@@ -0,0 +1,19 @@
+const benchmarker = (testFun, times = 1000000) => {
+  if (typeof testFun !== 'function') {
+    throw new Error('Did not provide a valid function for test.');
+  }
+
+  const startTime = new Date().getTime();
+
+  let i = 0;
+  while (i < times) {
+    i++;
+    testFun();
+  }
+
+  const endTime = new Date().getTime();
+
+  return endTime - startTime;
+};
+
+export default benchmarker;
